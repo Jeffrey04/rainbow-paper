608e510c50b2525eb00e68977b9b46304aaff0dc
Jeffrey04 <mycyberpet@yahoo.com>
2016-06-20 15:08:33 +0800

    rearrange markup and start responsive

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php wp_head(); ?>
 </head>
 <body>
     <div id="container">
+        <?php get_sidebar(); ?>
