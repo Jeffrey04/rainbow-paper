528a09ad0775866fdc063e62f5abb02301bf0f3c
Jeffrey04 <mycyberpet@yahoo.com>
2016-06-20 15:11:57 +0800

    separate header out of sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,16 @@
         <nav id="meta">
-            <ul class="sub">
-                <li id="header" class="widget">
-                    <h2><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h2>
-                    <p><?php bloginfo('name'); ?></p>
-                </li>
-                <?php
-                    if (is_active_sidebar('sidebar')):
-                        dynamic_sidebar('sidebar');
-                    endif;
-                ?>
+            <div id="header">
+                <script type="text/template" id="template-header-toggle">
+                    <i class="material-icons toggle">menu</i>
+                </script>
+                <h2><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h2>
+                <p><?php bloginfo('name'); ?></p>
+            </div>
+            <ul id="sidebar">
+            <?php
+                if (is_active_sidebar('sidebar')):
+                    dynamic_sidebar('sidebar');
+                endif;
+            ?>
             </ul>
         </nav>
