fea990f56df1981c0702eb1c840310455c3d954d
Jeffrey04 <mycyberpet@yahoo.com>
2016-06-21 16:52:22 +0800

    add rdfa prefixes

--- header.php
+++ header.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <?php wp_head(); ?>
 </head>
-<body>
+<body prefix="dc: http://purl.org/dc/terms/ schema: http://schema.org/ foaf: http://xmlns.com/foaf/0.1/">
     <div id="container">
         <?php get_sidebar(); ?>
