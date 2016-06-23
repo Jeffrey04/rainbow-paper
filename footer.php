3f5eb14983a685a143e169f9e7033f12de871219
Jeffrey04 <mycyberpet@yahoo.com>
2016-06-23 11:10:53 +0800

    footer copyright notice

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-        <div id="footer"></div>
+        <div id="footer"><p>Published posts are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.</p></div>
     </div>
     <?php wp_footer(); ?>
 </body>
